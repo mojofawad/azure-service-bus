9b26616e4800c7846a1c4052c147204f9a600962
mojofawad <mojofawad@gmail.com>
2026-08-15 16:11:37 -0400

    receive messages with newly created Receiver.cs

--- src/PrototypeApi/Receiver.cs
+++ src/PrototypeApi/Receiver.cs
@@ -1,9 +1,34 @@
+using Azure.Messaging.ServiceBus;
+
 namespace PrototypeApi;
 
-public class Receiver : BackgroundService
+public class Receiver(ServiceBusClient client) : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var processor = client.CreateProcessor("widget-queue", new ServiceBusProcessorOptions());
+
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        await processor.StartProcessingAsync(cancellationToken);
+
+        await Task.Delay(Timeout.Infinite, cancellationToken);
+    }
+
+    private async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        var body = args.Message.Body.ToString();
+        
+        Console.WriteLine($"Received: {body}");
+
+        await args.CompleteMessageAsync(args.Message);
+    }
+
+    private Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+
+        return Task.CompletedTask;
     }
 }
