6a10cf7c44d8fce1f580f7d728bf3d5d2c14e9d0
mojofawad <mojofawad@gmail.com>
2026-08-15 15:54:21 -0400

    create receiver background service

---
+++ src/PrototypeApi/Receiver.cs
@@ -0,0 +1,9 @@
+namespace PrototypeApi;
+
+public class Receiver : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
