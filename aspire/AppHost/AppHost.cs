fdcacbec2ed90174e65ed3ca8349aa87b735af82
Joey Fawad <54484850+mojofawad@users.noreply.github.com>
2026-08-15 22:58:03 -0400

    feat: configure service bus emulator in AppHost (#1)

--- aspire/AppHost/AppHost.cs
+++ aspire/AppHost/AppHost.cs
@@ -1,10 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var serviceBus = builder.AddAzureServiceBus("messaging")
+    .RunAsEmulator();
+
+serviceBus.AddServiceBusQueue("widget-queue");
+var topic = serviceBus.AddServiceBusTopic("widget-topic");
+topic.AddServiceBusSubscription("widget-subscription");
+
 builder.AddProject<Projects.Console>("console");
 
 var api = builder.AddProject<Projects.PrototypeApi>("server")
     .WithHttpHealthCheck("/health")
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WithReference(serviceBus);
 
 var webfrontend = builder.AddViteApp("webfrontend", "../../src/frontend")
     .WithReference(api)
