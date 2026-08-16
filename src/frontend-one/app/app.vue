b3ac5db87e5882930ec53fd8e743ee3c2c8aafd8
mojofawad <mojofawad@gmail.com>
2026-08-16 17:14:33 -0400

    add NuxtUI to nuxt project

--- src/frontend-one/app/app.vue
+++ src/frontend-one/app/app.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UApp>
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+    </UApp>
   </div>
 </template>
